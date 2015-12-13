d0d42b634079968035c42cecae79823bdcd054fd
RobertAKARobin <robertgfthomas@gmail.com>
2015-12-13 02:04:05 -0500

    added redirects to routes and deleted html folder

--- mhtml-sinatra.rb
+++ mhtml-sinatra.rb
@@ -2,14 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 require "httparty"
 
-get "/:page.json" do
-  "json"
-end
-
-get "/:page?" do
-  File.read("public/html/index.html")
+get "/:page?.?:format?" do
+  filepath = path_to params[:page]
+  if !params[:page]
+    File.read path_to "index"
+  elsif filepath && params[:page] != "index"
+    File.read filepath
+  else
+    redirect "/"
+  end
 end
 
 post "/" do
 
+end
+
+private
+def path_to filename
+  return nil if !filename
+  path = "public/page/#{filename}.html"
+  return false if !File.exists?(path)
+  return path
 end
